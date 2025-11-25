1704c98ce6118bc5c000525c1d38f3981e1be639
redflitzi <astrid.hanssen@gmx.de>
2025-11-25 19:03:06 +0100

    removed defaultConfig{} from android{}, because couldnt get it to work

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,9 +1,11 @@
 //import com.android.build.api.dsl.androidLibrary
+//import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     // alias(libs.plugins.kotlinMultiplatform)
     kotlin("multiplatform") version "2.2.21"
+    // id("com.android.library")
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.vanniktech.mavenPublish)
     // id("org.jetbrains.kotlin.kapt")
@@ -26,15 +28,23 @@ kotlin {
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
 
-         withJava() // enable java compilation support
+        withJava() // enable java compilation support
         withHostTestBuilder {}.configure {}
         withDeviceTestBuilder {
             sourceSetTreeName = "test"
         }
+/*
+        defaultConfig {
+            //applicationId = "io.github.redflitzi.compactdecimals"
+            minSdk = 31
+            //targetSdk = 34
+            //versionCode = 1
+            //versionName = "1.0"
 
-        //defaultConfig {
-        //    testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
-        //}
+            //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+ */
 
         compilations.configureEach {
             compilerOptions.configure {
@@ -62,8 +72,7 @@ kotlin {
     }
 
     js {
-        browser {
-        }
+        browser()
         binaries.executable()
     }
 
@@ -75,6 +84,8 @@ kotlin {
 
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            //implementation(androidx.test.runner)
+            // testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
         }
 
         val wasmJsMain by getting {
